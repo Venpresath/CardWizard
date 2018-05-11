d5aef9160da2b81e03690a975621b62be0440bdf
achinok <37181010+achinok@users.noreply.github.com>
2018-05-11 15:56:42 -0400

    Add files via upload

--- midterm.js
+++ midterm.js
@@ -1,14 +1,9 @@
 $(() => {
     $("#cardPage").hide();
     $("#playAgain").hide();
-    $("#startGame").click(function(){
-        $("#startGame").hide();
-    });
-
-
-    $("#startGame").click(function(){
+    $("#startGame").click(function () {
         $("#cardPage").show();
-        $("#cardPage").css("background-color", "#ff000080");
+        $("#startGame").hide();
         startGame();
     });
 });
@@ -68,40 +63,52 @@ function startGame() {
     let winCount = 4;
     let loseCount = 3;
     let guesses = [];
+    let choices = [];
 
     //Make it so the player can only select one card at a time.
     let selectedCard = $(".card").on("click", function () {
         selectedCard = $(this).find('p').text();
+        cardIndex = $(this).index();
+        choices.push(cardIndex);
+        console.log(choices);
+        $(this).toggleClass("active");
         guesses.push(selectedCard);
         console.log(guesses);
-        if (guesses.length === 2 && guesses[0] === guesses[1]) {
-            console.log("correct!");
-            winCount--
-            if (winCount === 0) {  
 
-                $("#playAgainButton").show(); 
-                $("#cancel").show();
+        if (choices.length === 2 && choices[0] === choices[1]) {
+            console.log("You cannot choose the same card");
+        } else {
+            runGame();
+        }
 
-                console.log("You win!");
-
-                //Pop up modal to ask user if they want to play again and run function below:
-
-
-                startGame();
-            }
-        } else if (guesses.length === 2 && guesses[0] !== guesses[1]) {
-            //if the cards don't match, make sure they flip down after 3 seconds (3000 ms)
-            console.log(`<p>try again, you have ${loseCount} times left</p>`); //delete this when you get the gameUpdate tag done.
-            loseCount--
-            // gameUpdate.innerHTML = `<p>try again, you have ${loseCount} times left</p>`; //update .gameUpdate tag here.
-            if (loseCount === 0) {
-                console.log("please try again");
-                startGame();
+        function runGame() {
+            if (guesses.length === 2 && guesses[0] === guesses[1]) {
+                console.log("correct!");
+                winCount--
+                if (winCount === 0) {
+                    // gameUpdate.innerHTML = `<b>You Win!!</b>`; //update .gameUpdate tag here.
+                    console.log("You win!");
+                    //Pop up modal to ask user if they want to play again and run function below:
+                    startGame();
+                }
+            } else if (guesses.length === 2 && guesses[0] !== guesses[1]) {
+                //if the cards don't match, make sure they flip down after 3 seconds (3000 ms)
+                console.log(`<b>try again, you have ${loseCount} times left</b>`); //delete this when you get the gameUpdate tag done.
+                loseCount--
+                if (loseCount === 0) {
+                    console.log("please try again");
+                    //ask user if they want to play again and run function below:
+                    startGame();
+                }
             }
         }
 
         if (guesses.length >= 2) {
-            guesses.splice(0, 3);
+            guesses.splice(0, 2);
+        }
+
+        if (choices.length >= 2) {
+            choices.splice(0, 2);
         }
     });
 }
